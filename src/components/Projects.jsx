7328d49ced78b87ded912a12cba2c50043d4afd4
Aditya Sengar <2k23.csaiml2311761@gmail.com>
2026-06-19 22:55:51 +0530

    clickable functionality on Project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,58 +37,62 @@ const Projects = () => {
         <div className='mt-4 text-4xl font-bold'>Check out my latest work</div>
 
         <div className='grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-200 mx-auto auto-rows-fr mt-5' >
-          <div className='flex flex-col border border-solid border-gray-200 rounded-xl '>
-            <div>
-             <img className='border-b border-gray-200 rounded-t-xl' src={portfolioImg} alt="" />
+          <a href="https://adityasengar.vercel.app/">
+            <div className='flex flex-col border border-solid border-gray-200 rounded-xl '>
+              <div>
+              <img className='border-b border-gray-200 rounded-t-xl' src={portfolioImg} alt="" />
+              </div>
+              <div className='flex flex-col gap-2'>
+                <div className='flex flex-col items-start pt-2 pl-4'>
+                  <h3 className='font-semibold'>PortfolioV01</h3>
+                  <time className='font-light text-gray-500 text-[13px]'>March 2026</time>
+                </div>
+                <div className='pl-4 flex items-start text-[14px]'>
+                  <p className='text-left text-gray-500 font-light'>
+                    Designed and developed a high-performance portfolio application with React, leveraging reusable UI components, and optimized user experience.
+                  </p>
+                </div>
+                <div className='pl-4 pr-2 flex flex-wrap gap-1'>
+                  {technologies_Portfolio.map((technology)=>(
+                    <div
+                    className='flex items-center gap-1 px-2 py-1 rounded-[8px] border border-gray-200 text-[10px] font-medium text-gray-800'
+                    >
+                      {technology.name}
+                    </div>
+                  ))}
+                </div>
+                <div className='p-1'></div>
+              </div>
             </div>
-            <div className='flex flex-col gap-2'>
-              <div className='flex flex-col items-start pt-2 pl-4'>
-                <h3 className='font-semibold'>PortfolioV01</h3>
-                <time className='font-light text-gray-500 text-[13px]'>March 2026</time>
+          </a>
+          <a href="https://travello-site.vercel.app/">
+            <div className='flex flex-col border border-solid border-gray-200 rounded-xl '>
+              <div>
+              <img className='border-b border-gray-200 rounded-t-xl' src={travelloImg} alt="" />
               </div>
-              <div className='pl-4 flex items-start text-[14px]'>
-                <p className='text-left text-gray-500 font-light'>
-                  Designed and developed a high-performance portfolio application with React, leveraging reusable UI components, and optimized user experience.
-                </p>
+              <div className='flex flex-col gap-2'>
+                <div className='flex flex-col items-start pt-2 pl-4'>
+                  <h3 className='font-semibold'>Travello</h3>
+                  <time className='font-light text-gray-500 text-[13px]'>November 2025</time>
+                </div>
+                <div className='pl-4 pr-4 flex items-start text-[14px]'>
+                  <p className='text-left text-gray-500 font-light'>
+                    A clean and modern web-based travel planner, helps users plan trips in a simple, intuitive, and visually polished way.
+                  </p>
+                </div>
+                <div className='pl-4 pr-2 flex flex-wrap gap-1'>
+                  {technologies_Travello.map((technology)=>(
+                    <div
+                    className='flex items-center gap-1 px-2 py-1 rounded-[8px] border border-gray-200 text-[10px] font-medium text-gray-800'
+                    >
+                      {technology.name}
+                    </div>
+                  ))}
+                </div>
+                <div className='p-1'></div>
               </div>
-              <div className='pl-4 pr-14 flex flex-wrap gap-1'>
-                {technologies_Portfolio.map((technology)=>(
-                  <div
-                  className='flex items-center gap-1 px-2 py-1 rounded-[8px] border border-gray-200 text-[10px] font-medium text-gray-800'
-                  >
-                    {technology.name}
-                  </div>
-                ))}
-              </div>
-              <div className='p-1'></div>
             </div>
-          </div>
-          <div className='flex flex-col border border-solid border-gray-200 rounded-xl '>
-            <div>
-             <img className='border-b border-gray-200 rounded-t-xl' src={travelloImg} alt="" />
-            </div>
-            <div className='flex flex-col gap-2'>
-              <div className='flex flex-col items-start pt-2 pl-4'>
-                <h3 className='font-semibold'>Travello</h3>
-                <time className='font-light text-gray-500 text-[13px]'>November 2025</time>
-              </div>
-              <div className='pl-4 pr-4 flex items-start text-[14px]'>
-                <p className='text-left text-gray-500 font-light'>
-                  A clean and modern web-based travel planner, helps users plan trips in a simple, intuitive, and visually polished way.
-                </p>
-              </div>
-              <div className='pl-4 pr-14 flex flex-wrap gap-1'>
-                {technologies_Travello.map((technology)=>(
-                  <div
-                  className='flex items-center gap-1 px-2 py-1 rounded-[8px] border border-gray-200 text-[10px] font-medium text-gray-800'
-                  >
-                    {technology.name}
-                  </div>
-                ))}
-              </div>
-              <div className='p-1'></div>
-            </div>
-          </div>
+          </a>
         </div>
     </div>
   )
